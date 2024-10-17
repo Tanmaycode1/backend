5a29d989bdacc0be196942ff1a1f124ae128131a
Tanmaycode1 <115889222+Tanmaycode1@users.noreply.github.com>
2024-10-17 15:38:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -10,13 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// Connect to MongoDB without deprecated options
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
